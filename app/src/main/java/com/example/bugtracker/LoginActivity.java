f39c3bb1653627e93a0dd1fbfc37deee8d17994b
Mark giliadov <markgiliadov@gmail.com>
2022-01-24 22:36:15 +0200

    Implementing basic registration activity with firebaseauth, firebase-realtime data

--- app/src/main/java/com/example/bugtracker/LoginActivity.java
+++ app/src/main/java/com/example/bugtracker/LoginActivity.java
@@ -2,16 +2,13 @@ package com.example.bugtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, StartActivity.class));
-        finish();
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/example/bugtracker/StartActivity.java
@@ -0,0 +1,79 @@
+package com.example.bugtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class StartActivity extends AppCompatActivity {
+
+    private ImageView iconImage;
+    private LinearLayout linearLayout;
+    private Button register;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        iconImage = findViewById(R.id.icon_image);
+        linearLayout = findViewById(R.id.linear_layout);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+        linearLayout.animate().alpha(0f).setDuration(1);
+
+        TranslateAnimation animation = new TranslateAnimation(0 ,0 ,0, -1000);
+        animation.setDuration(1600);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimationListener());
+
+        iconImage.setAnimation(animation);
+
+        // login on click
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                Toast.makeText(StartActivity.this, "HI", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // register on click
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                Toast.makeText(StartActivity.this, "HI", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener{
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            iconImage.clearAnimation();
+            iconImage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(400);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+}
